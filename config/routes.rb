e9a3e8340578babad9181ce8da114d6b9d970526
edoardo maraia <edoardo.maraia@gmail.com>
2020-11-16 22:14:10 +0100

    Add dotenv - Protect my secret data in .env file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :heros, only: [:new, :create, :show, :index]
+  resources :heros, only: [:new, :create, :show, :index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
