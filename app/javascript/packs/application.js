bad79bf2faeec02bdb16cbad2e224b27732b223c
Adham Jaafar <ajaafar65@gmail.com>
2020-11-20 14:28:48 +0100

    flat picker changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,9 +24,11 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import "flatpickr/dist/flatpickr";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+
 import { initMapbox } from '../plugins/init_mapbox';
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
